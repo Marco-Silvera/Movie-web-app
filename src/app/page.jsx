362849c66f0c78eb106611231232cabc5c99ef6d
Marco Silvera <msilveradev@gmail.com>
2025-05-16 09:03:02 -0500

    Implementar la obtención de películas populares, mejor valoradas y próximas en la página principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,15 +4,22 @@ import HeroHomePage from "@/components/HeroHomePage";
 import PopularSection from "@/components/PopularSection";
 import TopRatedSection from "@/components/TopRatedSection";
 import UpcomingRealeasesSection from "@/components/UpcomingReleasesSection";
+import { getPopularMovies, getTopRated, getUpComing } from "@/lib/tmdb";
 
-export default function Home() {
+export default async function Home() {
+    const popularMovies = await getPopularMovies();
+
+    const topRatedMovies = await getTopRated();
+
+    const upComingMovies = await getUpComing();
+
     return (
         <>
-            <HeroHomePage />
-            <PopularSection />
-            <TopRatedSection />
-            <UpcomingRealeasesSection />
-            <GenreSection />
+            <HeroHomePage movie={popularMovies[0]} />
+            <PopularSection movies={popularMovies} />
+            <TopRatedSection movies={topRatedMovies.slice(0, 3)} />
+            <UpcomingRealeasesSection movies={upComingMovies.slice(0, 4)} />
+            <GenreSection movies={popularMovies.slice(0, 12)} />
             <Contact />
         </>
     );
