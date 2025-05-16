7fd091d57521b7eda6f0a77eeb87a0f9e054a299
Marco Silvera <msilveradev@gmail.com>
2025-05-16 09:02:37 -0500

    Actualizando la gestión de llamadas a la API en tmdb.js para obtener películas populares, mejor valoradas y próximas, incluyendo la asignación de nombres de géneros.

--- src/lib/tmdb.js
+++ src/lib/tmdb.js
@@ -1,8 +1,76 @@
-const API_KEY = process.env.TMDB_API_KEY; // la API Key v3
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY; // la API Key v3
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export async function getPopularMovies() {
-    const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    if (!res.ok) throw new Error("Error al obtener películas populares");
-    return await res.json();
+    const [moviesRes, genresRes] = await Promise.all([
+        fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`),
+        fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`),
+    ]);
+
+    if (!moviesRes.ok || !genresRes.ok)
+        throw new Error("Error al obtener películas o géneros");
+
+    const moviesData = await moviesRes.json();
+    const genresData = await genresRes.json();
+
+    const genreMap = {};
+    genresData.genres.forEach((genre) => {
+        genreMap[genre.id] = genre.name;
+    });
+
+    const processedMovies = moviesData.results.map((movie) => ({
+        ...movie,
+        genre_names: movie.genre_ids.map((id) => genreMap[id]),
+    }));
+
+    return processedMovies;
+}
+
+export async function getTopRated() {
+    const [moviesRes, genresRes] = await Promise.all([
+        fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`),
+        fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`),
+    ]);
+    if (!moviesRes.ok || !genresRes.ok)
+        throw new Error("Error al obtener películas o géneros");
+
+    const moviesData = await moviesRes.json();
+    const genresData = await genresRes.json();
+
+    const genreMap = {};
+    genresData.genres.forEach((genre) => {
+        genreMap[genre.id] = genre.name;
+    });
+
+    const processedMovies = moviesData.results.map((movie) => ({
+        ...movie,
+        genre_names: movie.genre_ids.map((id) => genreMap[id]),
+    }));
+
+    return processedMovies;
+}
+
+export async function getUpComing() {
+    const [moviesRes, genresRes] = await Promise.all([
+        fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}`),
+        fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`),
+    ]);
+
+    if (!moviesRes.ok || !genresRes.ok)
+        throw new Error("Error al obtener películas o géneros");
+
+    const moviesData = await moviesRes.json();
+    const genresData = await genresRes.json();
+
+    const genreMap = {};
+    genresData.genres.forEach((genre) => {
+        genreMap[genre.id] = genre.name;
+    });
+
+    const processedMovies = moviesData.results.map((movie) => ({
+        ...movie,
+        genre_names: movie.genre_ids.map((id) => genreMap[id]),
+    }));
+
+    return processedMovies;
 }
